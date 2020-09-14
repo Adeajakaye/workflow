896c4c85d1b89bdc30455d526e36b0b6d5607268
Ajakaye Adedoyin <ajakayeadedoyin@gmail.com>
2020-09-14 12:21:07 -0700

    Add files via upload

---
+++ workflow.R
@@ -0,0 +1,32 @@
+2 + 2
+1 / 100 * 30
+sin(pi /2)
+sin(pi / 2)
+
+x <- 3 * 4
+x
+this_is_really_a_long_name <- 4 + 4
+this_is_really_a_long_name
+
+##calling function
+
+function_name(arg1 = val1, arg2 = val2)
+
+seq(1,10)
+
+x <- "hello world"
+x
+x <- "hello"
+x
+
+y= seq(1,10, length.out = 5)
+y
+(y <- seq(1,10, length.out = 6))
+
+library(tidyverse)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+fliter(mpg, cyl = 8)
+filter(diamond, carat > 3)
